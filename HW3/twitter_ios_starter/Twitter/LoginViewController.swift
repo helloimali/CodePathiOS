1e5863c24aad7d6e0e3728b2855437a979b9e29f
helloimali <toalisaeedmalik@gmail.com>
2021-03-06 19:17:59 -0500

    Button Creation + New file to control button

---
+++ HW3/twitter_ios_starter/Twitter/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Ali Malik on 3/6/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        //This will run when someone clicks the button
+        //Action, not an outlet
+        //print("ECHO")
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
