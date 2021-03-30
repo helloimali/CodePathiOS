0e4ac16f28a83f01e076c16e706277a30847b524
helloimali <toalisaeedmalik@gmail.com>
2021-03-30 16:35:26 -0400

    login

--- HW5/instagram/instagram/SceneDelegate.swift
+++ HW5/instagram/instagram/SceneDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Parse
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -17,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil{
+            // User is logged in
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavifationController = main.instantiateViewController(identifier: "feedNavController")
+            window?.rootViewController = feedNavifationController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
